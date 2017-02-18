f2d066d9040f8d97f191ee30f0a4b9911116d0f5
dragon8github <928532756@qq.com>
2017-02-19 02:31:14 +0800

    添加了单元测试

--- test/people.test.js
+++ test/people.test.js
@@ -1,2 +1,9 @@
-const spa_model = require('../src/js/spa.model.js');
-console.log(spa_model);
+const spa_model = require('../src/js/spa/spa.model.js');
+
+spa_model.initModule();  // 初始化数据，伪造数据
+
+var people_db = spa_model.people.get_db();
+
+var peopleList = people_db().get();
+
+console.log(peopleList);
