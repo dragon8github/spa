8975730215d6a6f7318aa9c35626ce8fee074e24
dragon8github <928532756@qq.com>
2017-02-19 09:21:14 +0800

    修补了model层

--- test/people.test.js
+++ test/people.test.js
@@ -7,3 +7,7 @@ var people_db = spa_model.people.get_db();
 var peopleList = people_db().get();
 
 console.log(peopleList);
+
+people_db().each( function (person, index) {
+	console.log(person.name, index);
+})
